f7b88e7cc6965701e7b0f80b9bafe97d25afbce7
Maciej Paluch <maciekpal@gmail.com>
2022-10-15 19:33:19 +0200

    feat: crypto chips added under route ./crypto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductsComponent } from './components/products/products.component';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { CryptoChipsComponent } from './components/crypto-chips/crypto-chips.component';
 import { ProductsComponentModule } from './components/products/products.component-module';
 import { ProductServiceModule } from './services/product.service-module';
 import { CategoriesComponentModule } from './components/categories/categories.component-module';
+import { CryptoChipsComponentModule } from './components/crypto-chips/crypto-chips.component-module';
+import { CryptoServiceModule } from './services/crypto.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'products', component: ProductsComponent }, { path: 'categories', component: CategoriesComponent }]), ProductsComponentModule, ProductServiceModule, CategoriesComponentModule],
+  imports: [RouterModule.forRoot([{ path: 'products', component: ProductsComponent }, { path: 'categories', component: CategoriesComponent }, { path: 'crypto', component: CryptoChipsComponent }]), ProductsComponentModule, ProductServiceModule, CategoriesComponentModule, CryptoChipsComponentModule, CryptoServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
