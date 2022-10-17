83cf134a0c754df6a588e6104bba3d5fe3834c47
Maciej Paluch <maciekpal@gmail.com>
2022-10-17 17:55:22 +0200

    feat: Exercise 5 - categories menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { CryptoChipsComponent } from './components/crypto-chips/crypto-chips.com
 import { HolidaysComponent } from './components/holidays/holidays.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { CategoriesCheckboxComponent } from './components/categories-checkbox/categories-checkbox.component';
+import { CategoriesMenuComponent } from './components/categories-menu/categories-menu.component';
 import { ProductsComponentModule } from './components/products/products.component-module';
 import { ProductServiceModule } from './services/product.service-module';
 import { CategoriesComponentModule } from './components/categories/categories.component-module';
@@ -15,6 +16,7 @@ import { HolidaysComponentModule } from './components/holidays/holidays.componen
 import { HolidaysServiceModule } from './services/holidays.service-module';
 import { HomePageComponentModule } from './components/home-page/home-page.component-module';
 import { CategoriesCheckboxComponentModule } from './components/categories-checkbox/categories-checkbox.component-module';
+import { CategoriesMenuComponentModule } from './components/categories-menu/categories-menu.component-module';
 
 @NgModule({
   imports: [RouterModule.forRoot([
@@ -23,8 +25,9 @@ import { CategoriesCheckboxComponentModule } from './components/categories-check
     { path: 'crypto', component: CryptoChipsComponent },
     { path: 'public-holidays', component: HolidaysComponent },
     { path: '', component: HomePageComponent },
-    { path: 'checkbox-categories', component: CategoriesCheckboxComponent }
-  ]), ProductsComponentModule, ProductServiceModule, CategoriesComponentModule, CryptoChipsComponentModule, CryptoServiceModule, HolidaysComponentModule, HolidaysServiceModule, HomePageComponentModule, CategoriesCheckboxComponentModule],
+    { path: 'checkbox-categories', component: CategoriesCheckboxComponent },
+    { path: 'categories-menu', component: CategoriesMenuComponent }
+  ]), ProductsComponentModule, ProductServiceModule, CategoriesComponentModule, CryptoChipsComponentModule, CryptoServiceModule, HolidaysComponentModule, HolidaysServiceModule, HomePageComponentModule, CategoriesCheckboxComponentModule, CategoriesMenuComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
